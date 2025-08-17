9a6ccbfa58d1a9520e3128a7baabb183b6c10291
J-D-K <gerplefart@gmail.com>
2025-08-17 15:34:00 -0400

    Add break timer, revisions.

--- Lift Timer/functions.js
+++ Lift Timer/functions.js
@@ -1,35 +1,53 @@
-let TIMER_DISPLAY = "timerDisplay"
-let HIGHLIGHT = "highlight"
+// NOTE: I'm not a JS dev. I'm a C/C++ dev. That's why this is actually readable. Not perfect, but readable.
+
+const TIMER_DISPLAY = "timerDisplay";
+const HIGHLIGHT = "highlight";
+const BREAK_TIME = 30;
 
 let beginTime = null;
+let breakTime = null;
 let timerInterval = null;
 
 function start_watch() {
     beginTime = Date.now();
-    timerInterval = setInterval(update, 10);
+    timerInterval = setInterval(update, 1000);
+}
+
+function start_break() {
+    const seconds = Math.floor(Date.now() / 1000);
+
+    breakTime = seconds + BREAK_TIME;
 }
 
 function update() {
-    let timerDisplay = document.getElementById(TIMER_DISPLAY);
-    let timeDiff = Date.now() - beginTime;
+    // Base time variables needed.
+    const timeCurrent = Date.now();
+    const currentSeconds = Math.floor(timeCurrent / 1000);
+    const timeDiff = timeCurrent - beginTime;
 
-    let totalSeconds = Math.floor(timeDiff / 1000);
-    let hours = Math.floor(totalSeconds / 3600);
-    let minutes = Math.floor((totalSeconds % 3600) / 60);
-    let seconds = totalSeconds % 60;
+    // Time displayed on clock.
+    const totalSeconds = Math.floor(timeDiff / 1000);
+    const hours = Math.floor(totalSeconds / 3600);
+    const minutes = Math.floor((totalSeconds % 3600) / 60);
+    const seconds = totalSeconds % 60;
 
-    let hoursString = hours.toString().padStart(2, "0");
-    let minutesString = minutes.toString().padStart(2, "0");
-    let secondsString = seconds.toString().padStart(2, "0");
-    let fullString = hoursString + ":" + minutesString + ":" + secondsString;
+    // Strings to concatenate together and display.
+    const hoursString = hours.toString().padStart(2, "0");
+    const minutesString = minutes.toString().padStart(2, "0");
+    const secondsString = seconds.toString().padStart(2, "0");
+    const fullString = `${hoursString}:${minutesString}:${secondsString}`;
 
+    // Grab and set the text.
+    const timerDisplay = document.getElementById(TIMER_DISPLAY);
     timerDisplay.textContent = fullString;
 
-    if (totalSeconds % 30 == 0) {
+    // These conditions control whether or not to flash red that a break has ended.
+    const breakHighlight = currentSeconds === breakTime && !timerDisplay.classList.contains(HIGHLIGHT);
+    const breakDone = currentSeconds !== breakTime && timerDisplay.classList.contains(HIGHLIGHT);
+    if (breakHighlight) {
         timerDisplay.classList.add(HIGHLIGHT);
     }
-    else {
+    else if (breakDone) {
         timerDisplay.classList.remove(HIGHLIGHT);
     }
-
 }
